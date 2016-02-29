35c3058a26f5b3beb3a75dae4f36563aeb0789eb
SaiWebApps <sairambkrishnan@gmail.com>
2016-02-28 18:38:32 -0800

    Source Code
    pollutantmean calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors.
    complete reads a directory full of files and reports the number of completely observed cases in each data file.
    corr takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold.

---
+++ complete.R
@@ -0,0 +1,25 @@
+complete <- function(directory, id = 1:332) {
+	## @param directory
+	##	Character vector of length 1 indicating the
+	##	location of the CSV files.
+	##
+	## @param id
+	##	Integer vector indicating monitor id numbers to be used.
+	##	Default value is 1:332 (seq_len(332)).
+	##
+	## @return
+	##	A data frame with columns "id" and "nobs," where id is the
+	##	monitor ID number, and nobs is the number of complete cases.	
+
+	output <- data.frame()
+
+	for (i in id) {
+		inputFilePath <- file.path(directory, sprintf("%03d.csv", i))
+		allInfo <- read.csv(inputFilePath)
+		numCompleteCases <- length(allInfo[complete.cases(allInfo), "ID"])
+		output <- rbind(output, c(i, numCompleteCases)) 
+	}
+
+	colnames(output) <- c("id", "nobs")
+	output
+}
---
+++ corr.R
@@ -0,0 +1,41 @@
+corr <- function(directory, threshold = 0) {
+	## @param directory
+	##	Character vector of length 1 indicating the
+	##	location of the CSV files.
+	##
+	## @param threshold
+	##	Numeric vector of length 1 indicating the number of
+	##	completely observed observations (on all variables)
+	##	required to compute the correlation between sulfate
+	##	and nitrate. Default value is 0. 
+	##
+	## @return
+	##	A numeric vector of correlations.
+	
+	pollutantCorr <- numeric()
+
+	# Retrieve a data frame delineating the number of complete
+	# observations for each monitor id in directory.
+	source("complete.R")
+	nobsTable <- complete(directory)
+
+	# Isolate the ids for which the number of complete observations
+	# is strictly greater than the threshold.
+	# Then, construct file paths for each of these filtered ids. 
+	targetIds <- nobsTable[nobsTable[,"nobs"] > threshold, "id"]
+	targetPaths <- file.path(directory, sprintf("%03d.csv", targetIds))
+
+	# Read the files corresponding to the filtered ids.
+	# For each file, calculate the correlation between complete (non-NA)
+	# sulfate and nitrate observations. Add the correlation to the
+	# pollutantCorr vector.
+	for (filePath in targetPaths) {
+		allInfo <- read.csv(filePath)
+		completeCases <- allInfo[complete.cases(allInfo),]
+		sulfate <- completeCases[,"sulfate"]
+		nitrate <- completeCases[,"nitrate"]
+		pollutantCorr <- c(pollutantCorr , cor(sulfate, nitrate))
+	}
+
+	pollutantCorr 
+}
