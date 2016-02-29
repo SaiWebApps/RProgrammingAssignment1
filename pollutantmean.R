01826664850175e55d2c48337200e77904e1cf5a
SaiWebApps <sairambkrishnan@gmail.com>
2016-02-28 18:39:29 -0800

    Moved comments outside function in pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,21 +1,20 @@
+## @param directory
+##	Character vector of length 1 indicating the
+##	location of the CSV files.
+##
+## @param pollutant
+##	Character vector of length 1 indicating the
+##	name of the pollutant for which we will calculate
+##	the mean; either "sulfate" or "nitrate".
+##
+## @param id
+##	Integer vector indicating monitor id numbers to be used.
+##	Default value is 1:332 (seq_len(332)).
+##
+## @return
+##	The mean of the pollutant across all monitors listed in
+##	the id vector (ignoring NA values).
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-	## @param directory
-	##	Character vector of length 1 indicating the
-	##	location of the CSV files.
-	##
-	## @param pollutant
-	##	Character vector of length 1 indicating the
-	##	name of the pollutant for which we will calculate
-	##	the mean; either "sulfate" or "nitrate".
-	##
-	## @param id
-	##	Integer vector indicating monitor id numbers to be used.
-	##	Default value is 1:332 (seq_len(332)).
-	##
-	## @return
-	##	The mean of the pollutant across all monitors listed in
-	##	the id vector (ignoring NA values).
-
 	pollutantInfo <- numeric()
 
 	inputFilePaths <- file.path(directory, sprintf("%03d.csv", id))
